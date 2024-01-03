617059fdbd98dfe2269bbc5131a153f18abf2352
Valorek <145722850+Valorek@users.noreply.github.com>
2024-01-03 13:00:57 +0100

    Update index.php

--- Sesje/index.php
+++ Sesje/index.php
@@ -1,5 +1,17 @@
 <?php
+
+//tworzenie sesji
 session_start();
 $_SESSION["login"]="edek";
 echo $_SESSION["login"];
+
+//generowanie i wyświetlanie id sesji
+
+session_start();
+session_regenerate_id();
+
+echo session_id()."<br>";
+session_regenerate_id();
+echo session_id();
+
 ?>
