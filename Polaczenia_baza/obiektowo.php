952e7d08299ced119764f8fb7e0999ff1591f4f2
Valorek <145722850+Valorek@users.noreply.github.com>
2024-02-21 14:12:14 +0100

    Update obiektowo.php

--- Polaczenia_baza/obiektowo.php
+++ Polaczenia_baza/obiektowo.php
@@ -21,40 +21,37 @@ while($wynik=$result->fetch_assoc());
 <?php
 $link=new mysqli("localhost","root","","Car_rental");
 $query="select * from klienci";
-$result = $link->query($query);
 
-while($wynik=$result->fetch_assoc());
-{
-  echo $wynik['imie']." ".$wynik['nazwisko'];
-  echo '<br>';
-  
+
+$result = $link->query($query);
+while($wynik=$result->fetch_assoc()){
+    echo $wynik['imie']." ".$wynik['nazwisko'];
+    echo "<br>";
+}
+?>
+
+
+<?php
+$link=new mysqli("localhost","root","","Car_rental");
+$query="select * from klienci";
+
+
+$result = $link->query($query);
+while($wynik=$result->fetch_row()){
+    echo $wynik[0]." ".$wynik[1];
+    echo "<br>";
 }
 ?>
 
 <?php
 $link=new mysqli("localhost","root","","Car_rental");
 $query="select * from klienci";
-$result = $link->query($query);
 
-while($wynik=$result->fetch_row());
-{
-  echo $wynik[1]." ".$wynik[2];
-  echo '<br>';
-  
+
+$result = $link->query($query);
+while($wynik=$result->fetch_array()){
+    echo $wynik[0]." ".$wynik['nazwisko'];
+    echo "<br>";
 }
 ?>
-
-<?php
-$link=new mysqli("localhost","root","","Car_rental");
-$query="select * from klienci";
-$result = $link->query($query);
-
-while($wynik=$result->fetch_array());
-{
-  echo $wynik[0]." ".$wynik['nazwisko'];
-  echo '<br>';
-  
-}
-?>
-
 
