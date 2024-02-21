3e4b7d225df4336df5c7cc37e653d982d7f4f435
Valorek <145722850+Valorek@users.noreply.github.com>
2024-02-21 13:47:25 +0100

    Update obiektowo.php

--- Polaczenia_baza/obiektowo.php
+++ Polaczenia_baza/obiektowo.php
@@ -6,3 +6,14 @@ echo $wynik = $result -> num_rows;
 echo "<br>";
 print_r($wynik=$result->fetch_assoc());
 ?>
+
+<?php
+$link=new mysqli("localhost","root","","Car_rental");
+$query="select * from klienci";
+$result = $link->query($query);
+while($wynik=$result->fetch_assoc());
+{
+  print_r($wynik);
+  echo '<br>';
+}
+?>
