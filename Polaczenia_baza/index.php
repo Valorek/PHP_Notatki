5c03158e6968a2826c747d7fc4f2e5b2ea4119ca
Valorek <145722850+Valorek@users.noreply.github.com>
2024-02-21 13:01:15 +0100

    Update index.php

--- Polaczenia_baza/index.php
+++ Polaczenia_baza/index.php
@@ -1,5 +1,5 @@
 <?php
-// $link =@mysqli_connect("localhost","root","","rental");
+$link =@mysqli_connect("localhost","root","","Car_rental");
 // if(!$link)
 // {
 //     echo "Blad";
@@ -7,14 +7,19 @@
 //     echo "Polaczenie ok!";
 // }
 
+$query="select * from Klienci";
+$result =mysqli_query($link,$query);
+echo $wynik = mysqli_num_rows($result);
 
-$link1=new mysqli('localhost','root','','Car_rental');
-if($link1->connect_error)
-{
-    echo 'Failed to connect to MySQL:' . $link1 -> connect_error;
-    exit();
-}
-else{
-    echo "Polaczenie ok!";
-}
+// $link1=new mysqli('localhost','root','','Car_rental');
+// if($link1->connect_error)
+// {
+//     echo 'Failed to connect to MySQL:' . $link1 -> connect_error;
+//     exit();
+// }
+// else{
+//     echo "Polaczenie ok!";
+// }
+
+
 ?>
