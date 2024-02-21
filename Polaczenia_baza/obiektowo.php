2490dca48713d2811a9c9293c56a5083543d146f
Valorek <145722850+Valorek@users.noreply.github.com>
2024-02-21 14:00:42 +0100

    Update obiektowo.php

--- Polaczenia_baza/obiektowo.php
+++ Polaczenia_baza/obiektowo.php
@@ -30,3 +30,18 @@ while($wynik=$result->fetch_assoc());
   
 }
 ?>
+
+<?php
+$link=new mysqli("localhost","root","","Car_rental");
+$query="select * from klienci";
+$result = $link->query($query);
+
+while($wynik=$result->fetch_row());
+{
+  echo $wynik[1]." ".$wynik[2];
+  echo '<br>';
+  
+}
+?>
+
+
